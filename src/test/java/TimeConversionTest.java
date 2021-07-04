f8c8cd8ccf89e54e0eb23251dc8c6d96c2bbfb52
Arne Jandt <arneybarneyhtbmail.com>
2021-07-04 18:50:39 +0200

    Time Conversion Test

--- src/test/java/TimeConversionTest.java
+++ src/test/java/TimeConversionTest.java
@@ -1,8 +1,10 @@
-package de.htwberlin;
-
+import de.htwberlin.TimeConversion;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class TimeConversionTest {
     private String input = "";
@@ -10,9 +12,40 @@ class TimeConversionTest {
     TimeConversion timeConversion = new TimeConversion(input);
 
     @Test
-    void timeConversion12To24() {
+    @DisplayName("Number one")
+    void timeConversion12To24One() {
         input = "07:05:45 PM";
         output = "19:05:45";
         assertEquals(output,timeConversion.timeConversion12To24(input));
+
     }
+
+
+    @Test
+    @DisplayName("Number two")
+    void timeConversion12To24Two(){
+        input = "07:05:32 AM";
+        output = "07:05:32";
+        assertEquals(output,timeConversion.timeConversion12To24(input));
+    }
+
+    @Test
+    @DisplayName("Number three")
+    void timeConversion12To24Three(){
+        input = "12:05:29 PM";
+        output = "12:05:29";
+        assertEquals(output,timeConversion.timeConversion12To24(input));
+    }
+
+    @Test
+    @DisplayName("Number four")
+    void timeConversion12To24Four(){
+        input = "12:05:00 AM";
+        output = "00:05:00";
+        assertEquals(output,timeConversion.timeConversion12To24(input));
+    }
+
+
+
+
 }
