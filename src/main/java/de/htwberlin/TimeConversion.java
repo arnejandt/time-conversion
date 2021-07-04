ea77e6f393fe88e9a7b854bb7d55d0002444702b
Arne Jandt <arneybarneyhtbmail.com>
2021-07-04 18:43:36 +0200

    TimeConversion Class

--- src/main/java/de/htwberlin/TimeConversion.java
+++ src/main/java/de/htwberlin/TimeConversion.java
@@ -1,2 +1,23 @@
-package de.htwberlin;public class TimeConversion {
+package de.htwberlin;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
+
+public class TimeConversion {
+    private String twelveHoursTime;
+
+
+    public TimeConversion(String twelveHoursTime) {
+        this.twelveHoursTime = twelveHoursTime;
+
+    }
+
+    public String timeConversion12To24(String twelveHoursTime) {
+        String result = LocalTime.parse(twelveHoursTime,
+                DateTimeFormatter.ofPattern("hh:mm:ss a", Locale.GERMANY))
+                .format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+        return result;
+    }
 }
